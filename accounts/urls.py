7f442840e51cfca5e00f45cedef0798bf3cedbc6
Anna Glazunova <79213278172@yandex.ru>
2022-09-13 18:08:32 +0400

    add

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,13 +4,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('login/',
-         LoginView.as_view(template_name = 'sign/login.html'),
+         LoginView.as_view(template_name = 'sign/loginin.html'),
          name='login'),
     path('logout/',
          LogoutView.as_view(template_name = 'sign/logout.html'),
          name='logout'),
     path('signup/',
-         BaseRegisterView.as_view(template_name = 'sign/signup.html'),
-         name='signup'),
+         BaseRegisterView.as_view(template_name = 'sign/signup_page.html'),
+         name='signup_page'),
     path('upgrade/', upgrade_me, name='upgrade'),
 ]
